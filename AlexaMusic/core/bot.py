e510720ab34ad9750fe96d38002ce4008feff975
Mehmet67676767 <kurt67143@gmail.com>
2025-05-12 17:06:10 +0300

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -17,6 +17,7 @@ import config
 from ..logging import LOGGER
 from pyrogram.enums import ChatMemberStatus
 
+
 class AlexaBot(Client):
     def __init__(self):
         super().__init__(
@@ -24,11 +25,9 @@ class AlexaBot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            sleep_threshold=30,
-            max_concurrent_transmissions=8,
-            workers=50,
+            max_concurrent_transmissions=5,
         )
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info("Starting Bot...")
 
     async def start(self):
         await super().start()
@@ -37,13 +36,10 @@ class AlexaBot(Client):
         self.id = get_me.id
         self.mention = get_me.mention
         try:
-            await self.send_video(
-                chat_id=config.LOG_GROUP_ID,
-                video="resources/startup.mp4",
-                caption="✅ Sıla bot Aktif!\n🎬 Asistan kontrol ediliyor...!",
-                supports_streaming=True
+            await self.send_message(
+                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
             )
-        except:
+        except Exception:
             LOGGER(__name__).error(
                 "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
@@ -53,7 +49,7 @@ class AlexaBot(Client):
             LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
             sys.exit()
         if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
+            self.name = f"{get_me.first_name} {get_me.last_name}"
         else:
             self.name = get_me.first_name
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
